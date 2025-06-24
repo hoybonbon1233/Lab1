886a98789a93a0c03f48b63f78ce8fef93c4fead
Thitiwat Pimpaeng <kinghoop12345@gmail.com>
2025-06-24 18:50:32 +0700

    Lab1
    ฐิติวัฒน์ ปิมแปง

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char Name[50] ;
+    int  Age = 0 ;
+    printf( "Enter your name: " ) ;
+    scanf( "%s", Name ) ;
+    printf( "Enter your age: " ) ;
+    scanf( "%d", &Age ) ;
+    printf( "Hello\n" ) ;
+    printf( "Name %s\n", Name ) ; 
+    printf( "Age = %d\n", Age ) ; 
+    return 0 ;
+}//end main function
